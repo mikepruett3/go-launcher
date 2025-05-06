668379b794441d39f8ac851e8769362a8dc43995
Mike Pruett <mikepruett3@gmail.com>
2025-05-06 13:00:26 -0500

    Tweeked the viper paths to look for a config file

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+//go:generate goversioninfo -icon=resource/icon.ico -manifest=resource/go-launcher.exe.manifest
+
 package main
 
 import (
@@ -50,15 +52,23 @@ func checkFileExists(filename string) bool {
 }
 
 func main() {
-	viper.SetConfigName("config")                           // name of config file (without extension)
-	viper.SetConfigType("yaml")                             // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("/etc/go-launcher/")                // path to look for the config file in
-	viper.AddConfigPath("$HOME/.config/go-launcher/")       // call multiple times to add many search paths
-	viper.AddConfigPath("$ENV:LOCALAPPDATA\\go-launcher\\") // ...
-	viper.AddConfigPath("$HOME\\.config\\go-launcher\\")    // ...
-	viper.AddConfigPath(".")                                // optionally look for config in the working directory
-	err := viper.ReadInConfig()                             // Find and read the config file
-	if err != nil {                                         // Handle errors reading the config file
+	// Retrieve the ProgramFiles environment variable
+	UserProfile := os.Getenv("UserProfile")
+	if UserProfile == "" {
+		fmt.Println("Error: USERPROFILE environment variable not found")
+		return
+	}
+
+	viper.SetConfigName("config")                                 // name of config file (without extension)
+	viper.SetConfigType("yaml")                                   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/etc/go-launcher/")                      // path to look for the config file in
+	viper.AddConfigPath(UserProfile + "\\.config\\go-launcher\\") // call multiple times to add many search paths
+	viper.AddConfigPath("$HOME/.config/go-launcher/")             // ...
+	viper.AddConfigPath("$ENV:LOCALAPPDATA\\go-launcher\\")       // ...
+	viper.AddConfigPath("$HOME\\.config\\go-launcher\\")          // ...
+	viper.AddConfigPath(".")                                      // optionally look for config in the working directory
+	err := viper.ReadInConfig()                                   // Find and read the config file
+	if err != nil {                                               // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
 
